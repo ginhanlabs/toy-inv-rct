34b7b9c0a240df8a49c24266b9f3dd4b35c81117
ginhanlabsgit config --global user.name ginhanlabsgit config --global user.name ginhanlabs <ginhanlabs@gmail.com>
2020-08-25 20:37:55 -0400

    add hover on cards

--- src/components/mainContent/mainContent.js
+++ src/components/mainContent/mainContent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import classes from './mainContent.module.css';
+import classes from './mainContent.module.css';
 
 const main = (props) => {
     const cardStyle = {
@@ -10,8 +10,8 @@ const main = (props) => {
 
     return (
         <div className="row">
-            <div>
-                <div className="card" style={cardStyle}  onClick={() => props.click('Action Figure')}>
+            <div >
+                <div className={classes.card} style={cardStyle}  onClick={() => props.click('Action Figure')}>
                     <div className="card-body">
                         <h5 className="card-title">
                             Action Figures
@@ -21,21 +21,21 @@ const main = (props) => {
                 </div>
             </div>
             <div >
-                <div className="card" style={cardStyle}  onClick={() => props.click('Statues')}>
+                <div className={classes.card} style={cardStyle}  onClick={() => props.click('Statues')}>
                     <div className="card-body">
                         <h5 className="card-title">Statues</h5>
                     </div>
                 </div>
             </div>
             <div>
-                <div className="card" style={cardStyle}  onClick={() => props.click('Busts')}>
+                <div className={classes.card} style={cardStyle}  onClick={() => props.click('Busts')}>
                     <div className="card-body">
                         <h5 className="card-title">Busts</h5>
                     </div>
                 </div>
             </div>
             <div>
-                <div className="card" style={cardStyle}  onClick={() => props.click('Dioramas')}>
+                <div className={classes.card} style={cardStyle}  onClick={() => props.click('Dioramas')}>
                     <div className="card-body">
                         <h5 className="card-title">Dioramas</h5>
                     </div>
